d73baeef9037194a34e685b89a8b4ee2be24318f
Satish N V R N <satish.nvrn@aithinkers.com>
2018-03-03 20:57:28 +0530

    Adds Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,10 +2,37 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 // import PropTypes from 'prop-types';
 
+import bloxOffice from 'assets/images/bloxoffice.png';
+
 import './Header.scss';
 
 const Header = () => (
-  <div>
+  <div className="header-container">
+    <div className="header-content">
+      <div className="box-1">
+        <img src={bloxOffice} alt="BloxOffice" className="main-logo" />
+      </div>
+      <div className="box-1 links-container">
+        <div className="link-items">
+          About
+        </div>
+        <div className="link-items">
+          Whitepaper
+        </div>
+        <div className="link-items">
+          Tokenomics
+        </div>
+        <div className="link-items">
+          Roadmap
+        </div>
+        <div className="link-items">
+          Team
+        </div>
+      </div>
+      <div className="login-btn-container">
+        Login/Signup
+      </div>
+    </div>
   </div>
 );
 
