0818660d538fb8b55d01439f7ce3437341853122
Satish N V R N <satish.nvrn@aithinkers.com>
2018-03-22 12:05:42 +0530

    Fixes header in mobile

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,35 +6,62 @@ import bloxOffice from 'assets/images/bloxoffice@3x.png';
 
 import './Header.scss';
 
-const Header = ({ handleElScroll }) => (
-  <div className="header-container">
-    <div className="header-content">
-      <div className="box-1">
-        <img src={bloxOffice} alt="BloxOffice" className="main-logo" />
-      </div>
-      <div className="box-1 links-container">
-        <div className="link-items">
-          <button className="link-btn" onClick={() => { handleElScroll('about'); }}>About</button>
-        </div>
-        <div className="link-items">
-          <button className="link-btn" onClick={() => { handleElScroll('whitepaper'); }}>Whitepaper</button>
-        </div>
-        <div className="link-items">
-          <button className="link-btn" onClick={() => { handleElScroll('tokenomics'); }}>Tokenomics</button>
-        </div>
-        {/* <div className="link-items">
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isMenuOpen: false,
+    };
+  }
+
+  toggleMenu = () => {
+    this.setState({
+      isMenuOpen: !this.state.isMenuOpen,
+    });
+  };
+
+  render() {
+    const { handleElScroll } = this.props;
+    const { isMenuOpen } = this.state;
+
+    return (
+      <div className="header-container">
+        <div className="header-content">
+          <div className="box-1">
+            <img src={bloxOffice} alt="BloxOffice" className="main-logo" />
+            <button className="menu-icon-btn" onClick={this.toggleMenu}>
+              <span
+                className="fa fa-bars menu-icon"
+              />
+            </button>
+          </div>
+          <div className={`menu-links-container ${isMenuOpen ? 'open' : ''}`}>
+            <div className="box-1 links-container">
+              <div className="link-items">
+                <button className="link-btn" onClick={() => { handleElScroll('about'); }}>About</button>
+              </div>
+              <div className="link-items">
+                <button className="link-btn" onClick={() => { handleElScroll('whitepaper'); }}>Whitepaper</button>
+              </div>
+              <div className="link-items">
+                <button className="link-btn" onClick={() => { handleElScroll('tokenomics'); }}>Tokenomics</button>
+              </div>
+              {/* <div className="link-items">
           <button className="link-btn" onClick={() => { handleElScroll('roadmap'); }}>Roadmap</button>
         </div> */}
-        <div className="link-items">
-          <button className="link-btn" onClick={() => { handleElScroll('team'); }}>Team</button>
-        </div>
-      </div>
-      {/* <div className="link-items login-btn-container">
+              <div className="link-items">
+                <button className="link-btn" onClick={() => { handleElScroll('team'); }}>Team</button>
+              </div>
+            </div>
+          </div>
+          {/* <div className="link-items login-btn-container">
         <Link to="/login"><p>Login/Signup</p></Link>
       </div> */}
-    </div>
-  </div>
-);
+        </div>
+      </div>
+    );
+  }
+}
 
 Header.defaultProps = {
   handleElScroll: () => {},
