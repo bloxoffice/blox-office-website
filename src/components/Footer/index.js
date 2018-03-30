0c15fbf4868daf8555bc7caec3d0c20c17abe384
killo <kranthikiranroy@gmail.com>
2018-03-30 17:00:06 +0530

    latest homepage fixes 1.1

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,9 +3,10 @@ import React from 'react';
 import bloxofficeFooter from 'assets/images/bloxoffice-footer@3x.png';
 import facebook from 'assets/images/facebook.svg';
 import twitter from 'assets/images/twitter.svg';
-import youtube from 'assets/images/youtube.svg';
+// import youtube from 'assets/images/youtube.svg';
 import medium from 'assets/images/medium.svg';
 import telegram from 'assets/images/telegram.svg';
+import gitHub from 'assets/images/github_2.svg';
 
 import './Footer.scss';
 
@@ -23,11 +24,11 @@ const Footer = () => (
         <div className="footer-link">CONTACT US</div>
       </div>
       <div className="box3">
-        <img href="google.com" src={medium} alt="medium" className="footer-share-icons" />
-        <img src={telegram} alt="telegram" className="footer-share-icons" />
-        <img src={twitter} alt="twitter" className="footer-share-icons" />
-        <img src={facebook} alt="facebook" className="footer-share-icons" />
-        <img src={youtube} alt="youtube" className="footer-share-icons" style={{ marginRight: 0 }} />
+        <a target="_blank" href="https://medium.com/bloxoffice"><img src={medium} alt="medium" className="footer-share-icons" /></a>
+        <a target="_blank" href="https://t.me/joinchat/HtwpshKacbV88nf7iTBtUQ"><img src={telegram} alt="telegram" className="footer-share-icons" /></a>
+        <a target="_blank" href="https://twitter.com/blox_office"><img src={twitter} alt="twitter" className="footer-share-icons" /></a>
+        <a target="_blank" href="https://www.facebook.com/bloxoffice"><img src={facebook} alt="facebook" className="footer-share-icons" /></a>
+        <a target="_blank" href="https://github.com/bloxoffice"><img src={gitHub} alt="gitHub" className="footer-share-icons-1" style={{ marginRight: 0 }} /></a>
       </div>
     </div>
   </div>
